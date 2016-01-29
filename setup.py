64364eb6dcf7ab1a6a84a1f2e81ae7b2ce210aff
Justin Huang <jstn@cs.washington.edu>
2016-01-28 18:59:35 -0800

    Fix catkin setup.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(packages=['location_server'],
+setup_args = generate_distutils_setup(packages=['location_db'],
                                       package_dir={'': 'src'})
 
 setup(**setup_args)
